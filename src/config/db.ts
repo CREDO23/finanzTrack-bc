ac6e5171cfc95203430108b1ce75d4753963d583
CREDO23 <thierrybakera12@gmail.com>
2023-11-05 10:12:08 +0200

    feat: add deployments env

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,15 +4,11 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-  {
-    dialectModule: pg,
-    dialect: 'postgres',
-    logging: false,
-  },
-);
+const sequelize = new Sequelize(`${process.env.DB_URI}`, {
+  dialectModule: pg,
+  dialect: 'postgres',
+  logging: false,
+});
 
 const dbConnection = async () => {
   try {
